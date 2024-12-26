4b1babd2697ab88d8f579674ab090b943cb2b905
VincTik <your.github@email.com>
2024-12-26 18:19:40 +0100

    Correction de l'erreur d'authentification de la carte

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -66,8 +66,8 @@ export default function MapComponent({ pointsRelais, onSelectPoint, selectedPoin
         className="z-0"
       >
         <TileLayer
-          url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
-          attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+          url="https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           maxZoom={20}
         />
         <MapUpdater points={pointsRelais} />
